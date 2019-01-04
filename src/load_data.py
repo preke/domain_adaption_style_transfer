2343f5742fec87fcad56b8957c79a33140ebb536
preke <geek00021@gmail.com>
2019-01-04 12:52:02 +0800

    .

--- src/load_data.py
+++ src/load_data.py
@@ -15,13 +15,13 @@ def preprocess(in_path, pos_output_paths, neg_output_paths):
         for line in reader:
             if line[9] == '1': # negative
                 text = line[11:]
-                neg_writer.write(text.split(': ')[0].lower())
-                neg_writer.write('\t')
+                # neg_writer.write(text.split(': ')[0].lower()) # title
+                # neg_writer.write('\t')
                 neg_writer.write(text.split(': ')[1].lower())
             if line[9] == '2': # positive
                 text = line[11:]
-                pos_writer.write(text.split(': ')[0].lower())
-                pos_writer.write('\t')
+                # pos_writer.write(text.split(': ')[0].lower()) # title
+                # pos_writer.write('\t')
                 pos_writer.write(text.split(': ')[1].lower())
     pos_writer.close()
     neg_writer.close()
