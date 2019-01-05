d34589ac7d7947b5429065d3cf51700bd0dd3715
preke <geek00021@gmail.com>
2019-01-05 20:11:56 +0800

    .

--- src/main.py
+++ src/main.py
@@ -63,7 +63,10 @@ embedding = initialWordEmbedding(GLOVE_PATH, w2i)
 
 # Train RGL()
 logger.info('Training RGL begin...')
-trainRGL()
+trainRGL(train_samples_batch,train_lenth_batch,train_labels_batch,train_mask_batch, \
+        dev_samples_batch,dev_lenth_batch,dev_labels_batch,dev_mask_batch, \
+        test_samples_batch,test_lenth_batch,test_labels_batch,test_mask_batch, \
+        vocab, w2i)
 
 
 
--- src/train.py
+++ src/train.py
@@ -52,7 +52,10 @@ def eval(samples,lenth,labels, model,alpha, masks, test = False):
                                                                            size))
     return accuracy,flag
 
-def  trainRGL():
+def  trainRGL(train_samples_batch,train_lenth_batch,train_labels_batch,train_mask_batch, \
+            dev_samples_batch,dev_lenth_batch,dev_labels_batch,dev_mask_batch, \
+            test_samples_batch,test_lenth_batch,test_labels_batch,test_mask_batch, \
+            vocab, w2i):
     rgl_net = RGLIndividualSaperateSC(len(vocab),300,2,300,embedding).cuda()
     # rgl_net = RGL.RGLCommonSaperateSC(len(vocab),300,2,300,embedding).cuda()
     # rgl_net = RGL.RGLIndividualSingleSC(len(vocab),300,2,300,embedding).cuda()
