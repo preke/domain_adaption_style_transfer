9877503e7f3710e7fd4ed29ccdd5c9d94b3994e9
preke <geek00021@gmail.com>
2019-01-11 00:39:26 +0800

    .

--- src/dataload.py
+++ src/dataload.py
@@ -44,7 +44,7 @@ def gen_iter(path, text_field, label_field, args):
 
 def load_data(train_path, dev_path, args):
     text_field  = data.Field(sequential=True, use_vocab=True, batch_first=True, 
-            lower=True, include_lengths=True, preprocessing=data.Pipeline(clean_str)
+            lower=True, include_lengths=True, preprocessing=data.Pipeline(clean_str),
             pad_token='<PAD>', unk_token='<UNK>', init_token='<SOS>', eos_token='<EOS>')
     label_field = data.Field()
     
