dadd5876fcee62a359f8e294247ccb21e3b2ed29
preke <geek00021@gmail.com>
2019-01-10 18:08:24 +0800

    .

--- src/main.py
+++ src/main.py
@@ -58,11 +58,14 @@ logger.info('Loading data begin...')
 train_samples_batch,train_lenth_batch,train_labels_batch,train_mask_batch, \
 dev_samples_batch,dev_lenth_batch,dev_labels_batch,dev_mask_batch, \
 test_samples_batch,test_lenth_batch,test_labels_batch,test_mask_batch, \
-vocab, w2i = get_batches(small_pos_path, small_neg_path)
+vocab, w2i = get_batches(POS_TRAIN_PATH, NEG_TRAIN_PATH)
+# vocab, w2i = get_batches(small_pos_path, small_neg_path)
+
 
 # Initial word embedding
 logger.info('Initial word embedding begin...')
-embedding = initialWordEmbedding(small_glove_path, w2i)    
+# embedding = initialWordEmbedding(small_glove_path, w2i)    
+embedding = initialWordEmbedding(GLOVE_PATH, w2i)    
 
 # Train RGL()
 logger.info('Training RGL begin...')
