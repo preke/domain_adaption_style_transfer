c1da202af204e1b9d27e299fe53ae120bbbb11e3
preke <geek00021@gmail.com>
2019-01-10 15:22:18 +0800

    .

--- src/train.py
+++ src/train.py
@@ -91,8 +91,8 @@ def  trainRGL(train_samples_batch,train_lenth_batch,train_labels_batch,train_mas
             # reconstruct parts
             class_out, domain_out, out, reconstruct_out = rgl_net(feature, lenth, alpha, mask)
             batch_size      = len(train_samples_batch)
-            feature_iow     = Variable(torch.LongTensor(feature.contiguous().view(-1).unsqueeze(1)).cuda())
-            reconstruct_out = Variable(torch.LongTensor(reconstruct_out).cuda())
+            feature_iow     = Variable(torch.LongTensor(feature.contiguous().view(-1).unsqueeze(1))).cuda()
+            reconstruct_out = Variable(torch.LongTensor(reconstruct_out)).cuda()
             print type(reconstruct_out)
             print type(feature_iow)
             loss = F.cross_entropy(Variable(reconstruct_out), Variable(feature_iow))
