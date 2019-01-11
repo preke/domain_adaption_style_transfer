f1be366e408f35ad669ec936e92bae14b2caea6a
preke <geek00021@gmail.com>
2019-01-11 13:30:07 +0800

    .

--- src/train.py
+++ src/train.py
@@ -87,7 +87,8 @@ def trainRGL(train_iter, dev_iter, train_data, model, args):
             model.train()
             sample  = batch.text[0]
             length  = batch.text[1]
-            label   = batch.label            
+            label   = batch.label   
+            print label         
             p       = float(i + epoch * len_iter) / n_epoch / len_iter
             alpha   = 2. / (1. + np.exp(-10 * p)) - 1
             feature = Variable(sample)
