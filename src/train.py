1340d995e734e588ab888cb0355aeca88d28410d
preke <geek00021@gmail.com>
2019-01-15 19:00:30 +0800

    .

--- src/train.py
+++ src/train.py
@@ -162,7 +162,7 @@ def style_transfer(pos_iter, model, args):
         mask    = Variable(torch.FloatTensor(mask).cuda())
         feature = Variable(sample)
         feature01, feature02 = model.extractFeature(feature, length, mask)
-        for i in len(length):
+        for i in range(len(length)):
             pos_df.append([ total_cnt, length[i], feature[i], feature01[i], feature02[i] ])
             total_cnt += 1
     
@@ -174,7 +174,7 @@ def style_transfer(pos_iter, model, args):
         mask    = generate_mask(torch.max(length), length)
         mask    = Variable(torch.FloatTensor(mask).cuda())
         feature01, feature02 = model.extractFeature(feature, length, mask)
-        for i in len(length):
+        for i in range(len(length)):
             neg_df.append([ total_cnt, length[i], feature[i], feature01[i], feature02[i] ])
             total_cnt += 1
 
