4c09bc18c6232e6dea6e5b2de21ef952e244dc89
preke <geek00021@gmail.com>
2019-01-17 22:47:06 +0800

    .

--- src/model.py
+++ src/model.py
@@ -69,7 +69,10 @@ class Decoder(nn.Module):
 
             target = self.embed(target)
             
-            for i in range(target_len):                     
+            for i in range(target_len):   
+                print 'train'
+                print target[:, i]
+
                 prev_s       = self.decodercell(target[:, i], content, sentiment)
                 dec_h[:,i,:] = prev_s # .unsqueeze(1)
             outputs = self.dec2word(dec_h)
@@ -84,9 +87,10 @@ class Decoder(nn.Module):
             
             for i in range(int(torch.max(length))):
                 target = self.embed(target).squeeze(1)                             
+                print 'evaluate'
+                print target
                 prev_s = self.decodercell(target, content, sentiment)
                 output = self.dec2word(prev_s)
-                print output
                 outputs[:,i,:] = output
                 target = output.topk(1)[1]
 
