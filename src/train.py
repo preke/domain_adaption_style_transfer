37b32586b56e551cd1a9a3c8c5af0332675e235b
preke <geek00021@gmail.com>
2019-01-19 16:41:18 +0800

    .

--- src/train.py
+++ src/train.py
@@ -127,18 +127,18 @@ def trainRGL(train_iter, dev_iter, train_data, model, args):
             err = err_domain + err_label + lamda * out + reconstruct_loss
             err.backward()
             optimizer.step()
-            if i % 100 == 0:
-                pass
-                # show_reconstruct_results(dev_iter, model, args, i)
-                # acc, flag = eval(dev_iter, model, alpha)
-                # save_path = save_dir + "epoch_" + str(epoch) + "_batch_" + str(i) + "_acc_" + str(acc) +"_bestmodel.pt"
-                # if flag:
-                #     torch.save(model.state_dict(), save_path)
-                #     logger.info('Save model to ' + save_path)
-                #     logger.info('epoch: %d, [iter: %d / all %d], err_s_label: %f, err_s_domain: %f, err_t_domain: %f, err_ae: %f' \
-                #       % (epoch, i, len_iter, err_label.cpu().data.numpy(),
-                #          err_domain.cpu().data.numpy(), out, reconstruct_loss))
-            i += 1
+            # if  % 100 == 0:
+            #     pass
+            #     # show_reconstruct_results(dev_iter, model, args, i)
+            #     # acc, flag = eval(dev_iter, model, alpha)
+            #     # save_path = save_dir + "epoch_" + str(epoch) + "_batch_" + str(i) + "_acc_" + str(acc) +"_bestmodel.pt"
+            #     # if flag:
+            #     #     torch.save(model.state_dict(), save_path)
+            #     #     logger.info('Save model to ' + save_path)
+            #     #     logger.info('epoch: %d, [iter: %d / all %d], err_s_label: %f, err_s_domain: %f, err_t_domain: %f, err_ae: %f' \
+            #     #       % (epoch, i, len_iter, err_label.cpu().data.numpy(),
+            #     #          err_domain.cpu().data.numpy(), out, reconstruct_loss))
+            # i += 1
             cnt_batch += 1
             writer.close()
         cnt_epoch += 1
