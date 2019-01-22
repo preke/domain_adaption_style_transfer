9d7bf20aa5489a2ab56953173eff91fde6ba92f4
preke <geek00021@gmail.com>
2019-01-22 20:46:29 +0800

    .

--- src/script.py
+++ src/script.py
@@ -3,13 +3,13 @@ import random
 
 list_of_sent = []
 
-with open('pos.txt', 'r') as reader:
+with open('../data/pos.txt', 'r') as reader:
     for line in reader:
         new_line = '1\t' + line
         list_of_sent.append(new_line)
 
 
-with open('neg.txt', 'r') as reader:
+with open('../data/neg.txt', 'r') as reader:
     for line in reader:
         new_line = '0\t' + line
         list_of_sent.append(new_line)
@@ -20,11 +20,11 @@ with open('neg.txt', 'r') as reader:
 random.shuffle(list_of_sent)
 
 
-writer = open('amazon_small_train.tsv', 'w')
+writer = open('../data/amazon_small_train.tsv', 'w')
 for line in list_of_sent[:10000]:
     writer.write(line)
 writer.close()
-writer = open('amazon_test.tsv', 'w')
+writer = open('../data/amazon_test.tsv', 'w')
 for line in list_of_sent[:-1000]:
     writer.write(line)
 writer.close()
