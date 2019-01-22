f2d81316ca6cc44ffafe3f666bd4adf7a1c9c0d0
preke <geek00021@gmail.com>
2019-01-22 21:40:31 +0800

    .

--- src/train.py
+++ src/train.py
@@ -110,15 +110,15 @@ def trainRGL(train_iter, dev_iter, train_data, model, args):
             err_label   = loss_class(class_out, target)
             err_domain  = loss_domain(class_out, target)
             
-            # err = err_domain + err_label + lamda * out + reconstruct_loss
-            err = reconstruct_loss
+            err = err_domain + err_label + lamda * out + reconstruct_loss
+            # err = reconstruct_loss
             err.backward()
             optimizer.step()
-            if cnt_batch % 100 == 0:
-                show_reconstruct_results(dev_iter, model, args, cnt_batch)
+            if cnt_batch % 500 == 0:
                 acc, flag = eval(dev_iter, model, alpha)
                 save_path = save_dir + "epoch_" + str(epoch) + "_batch_" + str(cnt_batch) + "_acc_" + str(acc) +"_bestmodel.pt"
                 if flag:
+                    show_reconstruct_results(dev_iter, model, args, cnt_batch)
                     torch.save(model.state_dict(), save_path)
                     logger.info('Save model to ' + save_path)
                 logger.info('epoch: %d, [iter: %d / all %d], err_s_label: %f, err_s_domain: %f, err_t_domain: %f, err_ae: %f' \
