f908e35291dc691abb50dc10ed6f1c463ba87f2b
preke <geek00021@gmail.com>
2019-01-31 20:39:22 +0800

    .

--- src/train.py
+++ src/train.py
@@ -313,10 +313,10 @@ def style_transfer(pos_iter, neg_iter, model, args):
     length        = pos_df['length'][0].unsqueeze(0)
 
 
-    for neg in neg_df['feature1']:
-            # print neg
-            sim.append(F.cosine_similarity(pos.unsqueeze(0), neg.unsqueeze(0)))
-        max_index = int(np.argmax(np.array(sim)))
+    # for neg in neg_df['feature1']:
+    #         # print neg
+    #         sim.append(F.cosine_similarity(pos.unsqueeze(0), neg.unsqueeze(0)))
+    #     max_index = int(np.argmax(np.array(sim)))
 
     
     for i in range(5):
