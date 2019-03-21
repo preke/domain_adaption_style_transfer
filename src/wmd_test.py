b03904e676cab83f3888c73ca7e9b7ca239e792d
preke <geek00021@gmail.com>
2019-03-21 19:58:55 +0800

    .

--- src/wmd_test.py
+++ src/wmd_test.py
@@ -40,12 +40,12 @@ logger.info('Train word2vec model...\n')
 
 
 list_all = list_pos + list_neg
-w2v_model = Word2Vec(list_all, min_count=3)
+w2v_model = Word2Vec(list_all, min_count=1)
 
 sim_matrix = []
 for i in range(10):
     sim_matrix.append([])
-    for j in range(100):
+    for j in range(1000):
         sim_matrix[i].append(w2v_model.wmdistance(list_pos[i], list_neg[j]))
 
 logger.info('sim_matrix_shape: %d, %d\n' %(len(sim_matrix), len(sim_matrix[0])))
