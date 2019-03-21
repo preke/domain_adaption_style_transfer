b37ec11470b9798b4cffcc09be0faeeeee51eff8
preke <geek00021@gmail.com>
2019-03-21 16:15:13 +0800

    .

--- src/wmd_test.py
+++ src/wmd_test.py
@@ -45,7 +45,7 @@ w2v_model = Word2Vec(list_all, min_count=3)
 sim_matrix = []
 for i in range(10):
     sim_matrix.append([])
-    for j in range(len(list_neg)):
+    for j in range(10):
         sim_matrix[i].append(w2v_model.wmdistance(list_pos[i], list_neg[j]))
 
 logger.info('sim_matrix_shape: %d, %d\n' %(len(sim_matrix), len(sim_matrix[0])))
@@ -53,7 +53,9 @@ logger.info('sim_matrix_shape: %d, %d\n' %(len(sim_matrix), len(sim_matrix[0])))
 with open('../data/wmd_result', 'w') as writer:
     for i in range(10):
         writer.write('Pos: %s\n' %list_pos[i])    
+        print sim_matrix[i]
         neg_index = int(np.argmax(np.array(sim_matrix[i])))
+        print neg_index
         writer.write('Neg: %s\n' %list_neg[neg_index])    
 
 
