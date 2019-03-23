4b2006045e6fe31cff0e137d1ec25836d4ea30e3
preke <geek00021@gmail.com>
2019-03-23 17:09:17 +0800

    .

--- src/wmd_test.py
+++ src/wmd_test.py
@@ -10,55 +10,55 @@ logging.config.fileConfig(config_file, disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
 
+def get_wmd(list_pos, list_neg, pos_sentence_list, neg_sentence_list):
+    logger.info('Train word2vec model...\n')
+    list_all = list_pos + list_neg
+    w2v_model = Word2Vec(list_all, min_count=5)
 
-list_pos = []
-list_neg = []
-pos_sentence_list = []
-neg_sentence_list = []
-stop_words = stopwords.words('english')
-
-logger.info('Read in data...\n')
-with open('../data/pos.txt', 'r') as reader:
-    for line in reader:
-        pos_sentence_list.append(line)
-        new_line = line.lower().split()
-        new_line = [w for w in new_line if w not in stop_words]
-        list_pos.append(new_line)
-
-with open('../data/neg.txt', 'r') as reader:
-    for line in reader:
-        neg_sentence_list.append(line)
-        new_line = line.lower().split()
-        new_line = [w for w in new_line if w not in stop_words]
-        list_neg.append(new_line)
-
-logger.info('Train word2vec model...\n')
-
-
-list_all = list_pos + list_neg
-w2v_model = Word2Vec(list_all, min_count=5)
-
-# f1 = file('wmd.pkl', 'wb')
-sim_matrix = []
-for i in range(10):
-    sim_matrix.append([])
-    for j in range(20000):
-        sim_matrix[i].append(w2v_model.wmdistance(list_pos[i], list_neg[j]))
-
-logger.info('sim_matrix_shape: %d, %d\n' %(len(sim_matrix), len(sim_matrix[0])))
-
-with open('../data/wmd_result', 'w') as writer:
+    # f1 = file('wmd.pkl', 'wb')
+    sim_matrix = []
     for i in range(10):
-        writer.write('Pos: %s\n' %pos_sentence_list[i])    
-        #print sim_matrix[i]
-        neg_index = int(np.argmin(np.array(sim_matrix[i])))
-        #print neg_index
-        writer.write('Neg: %s\n' %neg_sentence_list[neg_index]) 
-        writer.write('\n')    
+        sim_matrix.append([])
+        for j in range(20000):
+            sim_matrix[i].append(w2v_model.wmdistance(list_pos[i], list_neg[j]))
+
+    logger.info('sim_matrix_shape: %d, %d\n' %(len(sim_matrix), len(sim_matrix[0])))
+
+    with open('../data/wmd_result', 'w') as writer:
+        for i in range(10):
+            writer.write('Pos: %s\n' %pos_sentence_list[i])    
+            #print sim_matrix[i]
+            neg_index = int(np.argmin(np.array(sim_matrix[i])))
+            #print neg_index
+            writer.write('Neg: %s\n' %neg_sentence_list[neg_index]) 
+            writer.write('\n')    
 
 
-# w2v_model.wmdistance
+def read_in_data():
+    list_pos = []
+    list_neg = []
+    pos_sentence_list = []
+    neg_sentence_list = []
+    stop_words = stopwords.words('english')
+    logger.info('Read in data...\n')
+    with open('../data/pos.txt', 'r') as reader:
+        for line in reader:
+            pos_sentence_list.append(line)
+            new_line = line.lower().split()
+            # new_line = [w for w in new_line if w not in stop_words]
+            list_pos.append(new_line)
 
+    with open('../data/neg.txt', 'r') as reader:
+        for line in reader:
+            neg_sentence_list.append(line)
+            new_line = line.lower().split()
+            # new_line = [w for w in new_line if w not in stop_words]
+            list_neg.append(new_line)
 
-# word mover's distance pairing
+    return list_pos, list_neg, pos_sentence_list, neg_sentence_list
+
+if __name__ == '__main__':    
+    list_pos, list_neg, pos_sentence_list, neg_sentence_list = read_in_data()
+    get_wmd(list_pos, list_neg, pos_sentence_list, neg_sentence_list)
+
 
