5c185544e05c9b7c019741f37249361640a5e182
preke <geek00021@gmail.com>
2019-03-23 21:50:13 +0800

    .

--- src/wmd_test.py
+++ src/wmd_test.py
@@ -1,7 +1,14 @@
+# coding = utf-8
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from gensim.models import Word2Vec
 import numpy as np
 import pickle
+import math
+
 # logging
 import logging
 import logging.config
@@ -9,6 +16,28 @@ config_file = 'logging.ini'
 logging.config.fileConfig(config_file, disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
+
+class Preprocess(object):
+    def __init__(self):
+        self.english_punctuations = [',', '.', ':', ';', '?', '(', ')', '[', ']', '&', '!', '*', '@', '#', '$', '%', '\'']  
+        self.stop = set(stopwords.words('english'))
+
+    def punctuate(self, text):
+        ans = ""
+        for letter in text:
+            if letter in self.english_punctuations:
+                ans += ' '
+            else:
+                ans += letter
+        return ans
+
+    def stem_and_stop_removal(self, text):
+        text = self.punctuate(text)
+        word_list = word_tokenize(text)
+        word_list = [i for i in word_list if i not in self.stop]
+#         lancaster_stemmer = LancasterStemmer()
+#         word_list = [lancaster_stemmer.stem(i) for i in word_list]
+        return word_list
 
 def get_wmd(list_pos, list_neg, pos_sentence_list, neg_sentence_list):
     logger.info('Train word2vec model...\n')
@@ -39,26 +68,79 @@ def read_in_data():
     list_neg = []
     pos_sentence_list = []
     neg_sentence_list = []
-    stop_words = stopwords.words('english')
+    preprocess = Preprocess()
     logger.info('Read in data...\n')
     with open('../data/pos.txt', 'r') as reader:
         for line in reader:
             pos_sentence_list.append(line)
-            new_line = line.lower().split()
+            new_line = line.lower()
+            new_line = preprocess.stem_and_stop_removal(new_line)
             # new_line = [w for w in new_line if w not in stop_words]
             list_pos.append(new_line)
 
     with open('../data/neg.txt', 'r') as reader:
         for line in reader:
             neg_sentence_list.append(line)
-            new_line = line.lower().split()
+            new_line = line.lower()
+            new_line = preprocess.stem_and_stop_removal(new_line)
             # new_line = [w for w in new_line if w not in stop_words]
             list_neg.append(new_line)
 
     return list_pos, list_neg, pos_sentence_list, neg_sentence_list
 
+
+def mask_style_words(list_pos, list_neg):
+    preprocess = Preprocess()
+    # build style lexicons
+    dict_pos = {}
+    dict_neg = {}
+    for line in list_pos:
+        for word in line:
+            try:
+                dict_pos[word] += 1
+            except:
+                dict_pos[word] = 1
+    for line in list_neg:
+        for word in line:
+            try:
+                dict_neg[word] += 1
+            except:
+                dict_neg[word] = 1
+       
+    pos_words = []
+    for k,v in dict_pos.items():
+        try:
+            tf_idf = float(v)*math.log(len(dict_neg)/(dict_neg[k] + 1))
+        except:
+            tf_idf = float(v)*math.log(len(dict_neg)/(0 + 1))
+        pos_words.append((k, tf_idf))
+    neg_words = []
+    for k,v in dict_neg.items():
+        try:
+            tf_idf = float(v)*math.log(len(dict_pos)/(dict_pos[k] + 1))
+        except:
+            tf_idf = float(v)*math.log(len(dict_pos)/(0 + 1))
+        neg_words.append((k, tf_idf))
+    
+    pos_words = sorted(pos_words, lambda x, y: cmp(x[1], y[1]), reverse=True)
+    neg_words = sorted(neg_words, lambda x, y: cmp(x[1], y[1]), reverse=True)
+
+    pos_words_set = set([i[0] for i in pos_words[:100]])
+    neg_words_set = set([i[0] for i in neg_words[:100]])
+    # print pos_words_set
+    # print neg_words_set
+    # mask in each sentence
+    list_pos = [[j if j not in pos_words_set else '<neutral>' for j in i ] for i in list_pos ]
+    list_neg = [[j if j not in neg_words_set else '<neutral>' for j in i ] for i in list_neg ]
+    # return masked sentence list
+    return pos_words_set, neg_words_set, list_pos, list_neg
+
+
+
+
 if __name__ == '__main__':    
     list_pos, list_neg, pos_sentence_list, neg_sentence_list = read_in_data()
-    get_wmd(list_pos, list_neg, pos_sentence_list, neg_sentence_list)
+    pos_words_set, neg_words_set, list_pos_new,list_neg_new = mask_style_words(list_pos, list_neg)
+    get_wmd(list_pos_new, list_neg_new, pos_sentence_list, neg_sentence_list)
 
 
